bad635ec545f376d19519d2fe06b584d2fccebed
shah-unwindhomes <95495154+shah-unwindhomes@users.noreply.github.com>
2024-09-19 14:42:56 -0700

    git ignore

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 import { WebSocketServer } from 'ws';
 import axios from "axios"
 
-
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
     console.log('received: %s', data);
   
-  const dt = {
-  model: "tinyllama",
-  prompt:  data.toString(),
-  stream: false
-};
-  const request = axios.post('http://localhost:11434/api/generate', dt)  
-    .then((response) => {
-        console.log(response)
-        ws.send(response.data.response) 
-    });
+    const dt = {
+      model: "tinyllama",
+      prompt:  data.toString(),
+      stream: false
+    };
+    
+    const request = axios.post('http://localhost:11434/api/generate', dt)  
+      .then((response) => {
+          console.log(response)
+          ws.send(response.data.response) 
+      });
 });
 
 ws.send('open')   
